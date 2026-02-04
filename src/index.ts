7b17a973a430436a7c7ff2e8c920212bf156359c
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2026-02-03 22:22:14 -0800

    Asserted Types for Root

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 let editor;
 
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|android|ipad|playbook|silk/i.test(navigator.userAgent) ) {
-	document.querySelector("#introduction").innerHTML = "BeepBox is an online tool for sketching and sharing instrumental music. Make sure that your volume is turned up, then press the play button!";
-	document.querySelector("#keyboard-instructions").style.display = "none";
-	document.querySelector("#mobile-instructions").style.display = "";
-	document.querySelector("#bar-editing").innerHTML = "Tap the boxes to move to a different part of the song, or tap on the currently selected box to swap which pattern is played during that part of the song.";
-	document.querySelector("#offline-instructions").innerHTML = 'You can use BeepBox when your device is offline if you put a shortcut to BeepBox on your home screen. <ul><li>Chrome: find the "Add to Home Screen" option in the ⋮ menu.</li><li>Firefox: find the "Add Page Shortcut" option in the ⋮ menu.</li><li>Safari: find the "Add to Home Screen" option in the bookmark menu.</li></ul>';
+	document.querySelector<HTMLParagraphElement>("#introduction")!.innerHTML = "BeepBox is an online tool for sketching and sharing instrumental music. Make sure that your volume is turned up, then press the play button!";
+	document.querySelector<HTMLDivElement>("#keyboard-instructions")!.style.display = "none";
+	document.querySelector<HTMLDivElement>("#mobile-instructions")!.style.display = "";
+	document.querySelector<HTMLSpanElement>("#bar-editing")!.innerHTML = "Tap the boxes to move to a different part of the song, or tap on the currently selected box to swap which pattern is played during that part of the song.";
+	document.querySelector<HTMLParagraphElement>("#offline-instructions")!.innerHTML = 'You can use BeepBox when your device is offline if you put a shortcut to BeepBox on your home screen. <ul><li>Chrome: find the "Add to Home Screen" option in the ⋮ menu.</li><li>Firefox: find the "Add Page Shortcut" option in the ⋮ menu.</li><li>Safari: find the "Add to Home Screen" option in the bookmark menu.</li></ul>';
 }
 
-function browserHasRequiredFeatures() {
+function browserHasRequiredFeatures(): boolean {
 	"use strict";
 	if (window.AudioContext == undefined && window.webkitAudioContext == undefined) {
 		return false;
@@ -27,22 +27,22 @@ function browserHasRequiredFeatures() {
 
 if (browserHasRequiredFeatures()) {
 	// Go ahead and load js beepbox editor interface:
-	var fileref = document.createElement("script");
+	var fileref: HTMLScriptElement = document.createElement("script");
 	fileref.setAttribute("type", "text/javascript");
 	fileref.addEventListener("load", function(event) {
-		editor = new beepbox.SongEditor(document.querySelector("#beepboxEditorContainer"));
+		editor = new beepbox.SongEditor(document.querySelector<HTMLDivElement>("#beepboxEditorContainer")!);
 	});
 	fileref.setAttribute("src", "beepbox_editor.min.js");
 	document.head.appendChild(fileref);
 } else {
-	document.querySelector("#beepboxEditorContainer").innerHTML = "Sorry, BeepBox doesn't support your browser. Try a recent version of Chrome, Firefox, Edge, Safari, or Opera.";
+	document.querySelector<HTMLDivElement>("#beepboxEditorContainer")!.innerHTML = "Sorry, BeepBox doesn't support your browser. Try a recent version of Chrome, Firefox, Edge, Safari, or Opera.";
 }
 
 // If the page was loaded with an old song version that old versions of BeepBox support,
 // update the links to the old versions so that they'll open the song.
 if (/^#[1-6]/.test(location.hash)) {
-	document.querySelector("#linkTo2_3").href += location.hash;
+	document.querySelector<HTMLAnchorElement>("#linkTo2_3")!.href += location.hash;
 }
 if (/^#[1-8]/.test(location.hash)) {
-	document.querySelector("#linkTo3_0").href += location.hash;
+	document.querySelector<HTMLAnchorElement>("#linkTo3_0")!.href += location.hash;
 }
